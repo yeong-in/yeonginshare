256a7373923445197681ee3c99459d01547b4a5f
hae song <pinetree3966@gmail.com>
2021-08-20 15:29:49 +0900

    된건가

---
+++ app/src/main/java/com/example/calendarviewexample/Setting.java
@@ -0,0 +1,24 @@
+package com.example.calendarviewexample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Setting extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.setting,container, false);
+
+        return view ;
+    }
+}
